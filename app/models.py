b138966fbd4e481c854c5c686563e76a20ac2fd7
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-09 18:49:10 +0300

    Created model

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from app import db
+
+class User(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
