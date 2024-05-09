725c0dfacbdb4bd15b4cae787eb8699ebd18ce22
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-09 21:09:57 +0300

    Made final touches

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,33 @@
 from flask import Flask
-from flask_aqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth_data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-db = SQLAlchemy(app)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-from app import routes
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.models import User
+
+    with app.app_context():
+        db.create_all()  # Create database tables based on the models
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .routes import main
+    app.register_blueprint(main)
+    
+
+    return app
+
+
+ 
