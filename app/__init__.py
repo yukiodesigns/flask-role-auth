0dd1f19a12f783a46f9f2a685631fac36d99bb24
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-09 18:51:59 +0300

    Created init file

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_aqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth_data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from app import routes
